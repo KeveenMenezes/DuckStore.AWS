303c5613d251e721172bdf921817220cc01bd6ef
Keveen Menezes <endykeven@gmail.com>
2025-03-08 18:55:00 -0300

    refactor: ultilizando o metodo WithHttpsEndpoint para chamar em https

--- src/DuckStore.AppHost/Program.cs
+++ src/DuckStore.AppHost/Program.cs
@@ -12,7 +12,7 @@ var catalogApi = builder.AddProject<Projects.Catalog_API>(
 builder.AddNpmApp("web-app-ang", "../DuckStore.WebApp.ANG")
     .WithReference(catalogApi)
     .WaitFor(catalogApi)
-    .WithHttpEndpoint(env: "PORT")
+    .WithHttpsEndpoint(env: "PORT")
     .WithExternalHttpEndpoints()
     .PublishAsDockerFile();
 
