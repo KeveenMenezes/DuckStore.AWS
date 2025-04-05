f94d6275094ff5a2058cb536cb6a3bff480cc2c3
Keveen Menezes <endykeven@gmail.com>
2025-04-05 17:09:23 -0300

    feat: adicionar eventos de criação e atualização de pedidos, enums de método de pagamento e status de pedido

--- src/DuckStore.BuildingBlocks/BuildingBlocks.Messaging/MassTransit/Extensions.cs
+++ src/DuckStore.BuildingBlocks/BuildingBlocks.Messaging/MassTransit/Extensions.cs
@@ -4,34 +4,29 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 
 namespace BuildingBlocks.Messaging.MassTransit;
-
-public static class Extensions
+public static class Extentions
 {
-    public static IServiceCollection AddMessageBroker(
-        this IServiceCollection services,
-        IConfiguration configuration,
-        Assembly? assembly = null)
+    public static IServiceCollection AddMessageBroker
+        (this IServiceCollection services, IConfiguration configuration, Assembly? assembly = null)
     {
         services.AddMassTransit(config =>
         {
-            if (assembly != null)
-            {
-                config.AddConsumers(assembly);
-            }
-
             config.SetKebabCaseEndpointNameFormatter();
+
+            if (assembly != null)
+                config.AddConsumers(assembly);
 
             config.UsingRabbitMq((context, configurator) =>
             {
-                configurator.Host(new Uri(configuration["MessageBroker: Host"]!), host =>
+                configurator.Host(new Uri(configuration["MessageBroker:Host"]!), host =>
                 {
-                    host.Username(configuration["MessageBroker: UserName"]!);
-                    host.Password(configuration["MessageBroker: Password"]!);
+                    host.Username(configuration["MessageBroker:UserName"]!);
+                    host.Password(configuration["MessageBroker:Password"]!);
                 });
-
                 configurator.ConfigureEndpoints(context);
             });
         });
+
         return services;
     }
 }
