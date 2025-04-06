d20ce3f556129c679d2fa79698e6e41c0b3eb7fc
Keveen Menezes <endykeven@gmail.com>
2025-04-06 02:20:08 -0300

    refactor: remover arquivos não utilizados e limpar estrutura do projeto

---
+++ src/AppHost/Program.cs
@@ -0,0 +1,82 @@
+﻿using AppHost.Extensions;
+
+var builder = DistributedApplication.CreateBuilder(args);
+
+builder.AddForwardedHeaders();
+
+// DataBase
+var redis = builder.AddRedis("redis");
+var discountDb = builder.AddSqlite("discountDb");
+var catalogDb = builder.AddPostgres("catalogDb");
+var basketDb = builder.AddPostgres("basketDb");
+var orderingDb = builder.AddSqlServer("orderingDb");
+
+// Messaging
+var rabbitmq = builder
+    .AddRabbitMQ(
+        "messageBroker",
+        builder.AddParameter("username", secret: true),
+        builder.AddParameter("password", secret: true),
+        5672)
+    .WithManagementPlugin();
+
+// Services
+var catalogApi = builder.AddProject<Projects.Catalog_API>(
+    "catalogapi", GetHttpForEndpoints())
+    .WithExternalHttpEndpoints()
+    .WaitFor(catalogDb)
+    .WaitFor(rabbitmq)
+    .WithReference(catalogDb)
+    .WithReference(rabbitmq)
+    .WithHttpsHealthCheck("/health");
+
+var discountapi = builder.AddProject<Projects.Discount_Grpc>(
+    "discountapi", GetHttpForEndpoints())
+    .WithExternalHttpEndpoints()
+    .WaitFor(discountDb)
+    .WithReference(discountDb);
+
+var basketApi = builder.AddProject<Projects.Basket_API>(
+    "basketapi", GetHttpForEndpoints())
+    .WithExternalHttpEndpoints()
+    .WaitFor(redis)
+    .WaitFor(basketDb)
+    .WaitFor(discountapi)
+    .WaitFor(catalogApi)
+    .WaitFor(rabbitmq)
+    .WithReference(redis)
+    .WithReference(basketDb)
+    .WithReference(discountapi)
+    .WithReference(catalogApi)
+    .WithReference(rabbitmq)
+    .WithHttpsHealthCheck("/health");
+redis.WithParentRelationship(basketApi);
+
+var orderingMigration = builder.AddProject<Projects.Ordering_MigrationService>("ordering-migration")
+    .WaitFor(orderingDb)
+    .WithReference(orderingDb);
+
+builder.AddProject<Projects.Ordering_API>(
+    "orderapi", GetHttpForEndpoints())
+    .WithExternalHttpEndpoints()
+    .WaitFor(orderingDb)
+    .WithReference(orderingMigration)
+    .WithReference(orderingDb)
+    .WithHttpsHealthCheck("/health");
+
+// Web app
+builder.AddNpmApp("shopping", "../WebApps/WebApp.ANG")
+    .WithExternalHttpEndpoints()
+    .WaitFor(catalogApi)
+    .WaitFor(basketApi)
+    .WithReference(catalogApi)
+    .WithReference(basketApi)
+    .WithHttpsEndpoint(env: "PORT")
+    .PublishAsDockerFile();
+
+await builder.Build().RunAsync();
+
+static string GetHttpForEndpoints() =>
+    int.TryParse(
+        Environment.GetEnvironmentVariable("ESHOP_USE_HTTP_ENDPOINTS"),
+        out int result) && result == 1 ? "http" : "https";
--- src/DuckStore.WebApp.ANG/src/app/shared/models/pagination.ts
+++
@@ -1,6 +0,0 @@
-export type Pagination<T> = {
-  pageIndex: number;
-  pageSize: number;
-  totalItems: number;
-  items: T[];
-};
