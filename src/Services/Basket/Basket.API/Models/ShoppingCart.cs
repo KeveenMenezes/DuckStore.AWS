dbee46357880531ca17278bd395456c5de02d367
Keveen Menezes <endykeven@gmail.com>
2025-04-06 04:25:05 -0300

    feat: adicionar endpoint de checkout e atualizar modelo de carrinho de compras

--- src/Services/Basket/Basket.API/Models/ShoppingCart.cs
+++ src/Services/Basket/Basket.API/Models/ShoppingCart.cs
@@ -13,6 +13,7 @@ public class ShoppingCart
     }
 
     public string UserName { get; set; }
+
     public List<ShopppingClassItem> Items { get; set; } = [];
 
     public decimal TotalPrice => Items.Sum(x => x.Price * x.Qantity);
