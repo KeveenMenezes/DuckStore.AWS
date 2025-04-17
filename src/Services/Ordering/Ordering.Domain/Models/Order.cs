07556a4558c2d257828836ee8a9ce804e0beefca
Keveen Menezes <endykeven@gmail.com>
2025-04-17 08:59:32 -0300

    feat: adicionar novos projetos de teste e atualizar referências de pacotes

--- src/Services/Ordering/Ordering.Domain/Models/Order.cs
+++ src/Services/Ordering/Ordering.Domain/Models/Order.cs
@@ -2,20 +2,7 @@ namespace Ordering.Domain.Models;
 
 public class Order : Aggregate<OrderId>
 {
-    private readonly List<OrderItem> _orderItems = [];
-    public IReadOnlyList<OrderItem> OrderItems => _orderItems.AsReadOnly();
-
-    public CustomerId CustomerId { get; private set; } = default!;
-    public OrderName OrderName { get; private set; } = default!;
-    public Address ShippingAddress { get; private set; } = default!;
-    public Address BillingAddress { get; private set; } = default!;
-    public Payment Payment { get; private set; } = default!;
-    public OrderStatus Status { get; private set; } = OrderStatus.Pending;
-    public decimal TotalPrice
-    {
-        get => OrderItems.Sum(x => x.Price * x.Quantity);
-        private set { }
-    }
+    private Order() { }
 
     public static Order Create(
         OrderId id,
@@ -73,5 +60,20 @@ public class Order : Aggregate<OrderId>
         {
             _orderItems.Remove(orderItem);
         }
+    }
+
+    private readonly List<OrderItem> _orderItems = [];
+    public IReadOnlyList<OrderItem> OrderItems => _orderItems.AsReadOnly();
+
+    public CustomerId CustomerId { get; private set; } = default!;
+    public OrderName OrderName { get; private set; } = default!;
+    public Address ShippingAddress { get; private set; } = default!;
+    public Address BillingAddress { get; private set; } = default!;
+    public Payment Payment { get; private set; } = default!;
+    public OrderStatus Status { get; private set; } = OrderStatus.Pending;
+    public decimal TotalPrice
+    {
+        get => OrderItems.Sum(x => x.Price * x.Quantity);
+        private set { }
     }
 }
