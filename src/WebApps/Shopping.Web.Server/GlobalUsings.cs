2efcb6f0ba68a9bc1227c579136278e450075f73
Keveen Menezes <endykeven@gmail.com>
2025-04-25 23:07:36 -0300

    teste

--- src/WebApps/Shopping.Web.Server/GlobalUsings.cs
+++ src/WebApps/Shopping.Web.Server/GlobalUsings.cs
@@ -3,7 +3,6 @@ global using BuildingBlocks.Pagination;
 global using BuildingBlocks.ServiceDefaults;
 global using Microsoft.AspNetCore.Mvc;
 global using Microsoft.AspNetCore.Mvc.RazorPages;
-global using Refit;
 global using Shopping.Web.Server.Models.Basket;
 global using Shopping.Web.Server.Models.Catalog;
 global using Shopping.Web.Server.Models.Ordering;
