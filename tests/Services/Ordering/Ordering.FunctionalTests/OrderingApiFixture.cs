470002d4096d609e5190fb068d6da4a832809e61
Keveen Menezes <endykeven@gmail.com>
2025-05-01 00:31:29 -0300

    feat: remover testes funcionais obsoletos e atualizar dependências do projeto Ordering

---
+++ tests/Services/Ordering/Ordering.FunctionalTests/OrderingApiFixture.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc.Testing;
+using Microsoft.Extensions.Hosting;
+using Microsoft.VisualStudio.TestPlatform.TestHost;
+
+namespace Ordering.FunctionalTests;
+
+public sealed class OrderingApiFixture : WebApplicationFactory<Program>, IAsyncLifetime
+{
+    private readonly IHost _app;
+
+    public Task InitializeAsync()
+    {
+        throw new NotImplementedException();
+    }
+
+    Task IAsyncLifetime.DisposeAsync()
+    {
+        throw new NotImplementedException();
+    }
+}
