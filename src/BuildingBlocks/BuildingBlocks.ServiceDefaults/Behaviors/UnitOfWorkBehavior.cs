7166b90d0a7b6b514727bf620fad18e39c850237
Keveen Menezes <endykeven@gmail.com>
2025-06-26 15:56:20 -0300

    feat: implement IUnitOfWork interface and UnitOfWork class for transaction management

---
+++ src/BuildingBlocks/BuildingBlocks.ServiceDefaults/Behaviors/UnitOfWorkBehavior.cs
@@ -0,0 +1,31 @@
+using BuildingBlocks.Core.Abstractions;
+
+namespace BuildingBlocks.ServiceDefaults.Behaviors;
+
+public class UnitOfWorkBehavior<TRequest, TResponse>(
+    IUnitOfWork unitOfWork
+)
+    : IPipelineBehavior<TRequest, TResponse>
+    where TRequest : notnull, IRequest<TResponse>
+    where TResponse : notnull
+{
+
+    public async Task<TResponse> Handle(TRequest request, RequestHandlerDelegate<TResponse> next, CancellationToken cancellationToken)
+    {
+        if (!IsCommand())
+        {
+            return await next();
+        }
+
+        var response = await next();
+
+        await unitOfWork.SaveChangesAsync(cancellationToken);
+
+        return response;
+    }
+
+    private static bool IsCommand()
+    {
+        return typeof(TRequest).Name.EndsWith("Command", StringComparison.OrdinalIgnoreCase);
+    }
+}
